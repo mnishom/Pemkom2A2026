29949d17922439bc6991bdcafc3b230c0dd9df2b
mnish <mnish@LAPTOP-9HAUE24K>
2026-04-16 12:14:35 +0700

    2ST Push

--- src/main/java/com/ituhn/pemkom2/MainApp.java
+++ src/main/java/com/ituhn/pemkom2/MainApp.java
@@ -4,17 +4,11 @@ import com.ituhn.pemkom2.objects.Karyawan;
 
 public class MainApp {
     public static void main(String[] args) {
-        Karyawan k = new Karyawan();
-        k.setUidRfid("12345678");
-        k.setIdKaryawan("321");
-        k.setNamaLengkap("Galuh"); 
-        k.setDepartemen("Teller"); 
-        
-//        String data = k.toString();
-        System.err.println(k.toString());
-        
-        
-        
-        
+        Karyawan KR = new Karyawan();
+        if(KR instanceof Karyawan){
+            System.err.println("Karyawan");
+        }else {
+            System.err.println("Something else");
+        }
     }
 }
