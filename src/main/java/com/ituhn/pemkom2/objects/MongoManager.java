f0c72be588206eb8a98bb748d0ea80a60e53eab9
mnish <mnish@LAPTOP-9HAUE24K>
2026-04-27 06:16:36 +0700

    3ST Push

--- src/main/java/com/ituhn/pemkom2/objects/MongoManager.java
+++ src/main/java/com/ituhn/pemkom2/objects/MongoManager.java
@@ -4,9 +4,13 @@
  */
 package com.ituhn.pemkom2.objects;
 
+import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
 
 /**
  *
@@ -21,8 +25,17 @@ public class MongoManager {
 
     public static MongoDatabase getDatabase() {
         if (mongoClient == null) {
-            // Menggunakan default connection string untuk MongoDB versi 5.0.0
+            // Konfigurasi CodecRegistry untuk pemetaan POJO otomatis (Standard Industry)
+            CodecRegistry pojoCodecRegistry = CodecRegistries.fromRegistries(
+                MongoClientSettings.getDefaultCodecRegistry(),
+                CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build())
+            );
+
+            // Inisiasi koneksi ke MongoDB Localhost (Driver 5.0.0)
             mongoClient = MongoClients.create("mongodb://localhost:27017");
+            
+            // Mengembalikan database dengan registry yang sudah dikonfigurasi
+            return mongoClient.getDatabase(DATABASE_NAME).withCodecRegistry(pojoCodecRegistry);
         }
         return mongoClient.getDatabase(DATABASE_NAME);
     }
